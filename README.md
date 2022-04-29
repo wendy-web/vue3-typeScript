0c3bf843cbf2463b1263838b85c00989870f4dbe
v_congxwen <v_congxwen@tencent.com>
2022-04-29 17:30:54 +0800

    feat: 初始化webpack的项目

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-first
+初始化项目：npm init
+安装： npm install webpack vue vue-loader
+npm i css-loader vue-template-compiler
---
+++ src/app.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>{{text}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: "abc"
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
